f4383e236fc08f5504413065a16137c0fa9fc4da
4alejo0428 <josealejo3228@gmail.com>
2025-06-20 09:25:11 -0500

    feat: update sistemainventario

--- agregar_producto.php
+++ agregar_producto.php
@@ -10,7 +10,7 @@ include 'includes/obtener_categorias.php';
     <title>Agregar Producto</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body class="bg-light">
+<body class="bg-light"> 
 <div class="container mt-5">
     <h2 class="mb-4">Agregar Producto al Inventario</h2>
 
